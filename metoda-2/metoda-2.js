a58d4eedc0d654966d0ac8f374e32df074439da8
Tymoteusz Nachtman <tynach99@gmail.com>
2020-11-02 11:19:47 +0100

    Dodano dostosowanie liczby firm

--- metoda-2/metoda-2.js
+++ metoda-2/metoda-2.js
@@ -3,6 +3,7 @@
 */
 let counter = 0;
 let company_counter = 0;
+let company_start = 1;
 
 const GenerujURL = (firmy) => {
     let company_query = ``;
@@ -10,7 +11,6 @@ const GenerujURL = (firmy) => {
     for(let i=0; i<firmy.length; i++){
         company_query += `%22${firmy[i]}%22OR`;
         company_counter++;
-        console.log(company_counter);
     }
 
     // Usuwam tego ostatniego zbędnego OR'a
@@ -21,7 +21,7 @@ const GenerujURL = (firmy) => {
 }
 
 const GenerujNazwyFirm = (firmy) => {
-    return `Numery 1 - ${company_counter}`;
+    return `Numery ${company_start} - ${company_counter}`;
 }
 
 const GenerujDane = (firmy) => {
@@ -53,17 +53,87 @@ input.on('change', () => {
 
         let firmy = [];
         
-        // póki co do testowania używamy 45 pierwszych firm (nie powinno przekroczyć tego 1000 znaków)
-        for(let i=0; i<45; i++){ 
+        // UWAGA: ilość znaków w zapytaniu nie może przekroczyć 1000
+        let po_ile_firm = parseInt($('#po_ile_firm').val()); // pobierz ile firm brać pod uwagę w jednym zapytaniu ze strony (domyślnie 45)
 
-            // Tablica firm przesyłana do funkcji
-            firmy.push(lines[i].split('&').join('%26')); // trzeba zastąpić wszystkie '&' kodem URL
+        for(let j=0; j<(lines.length - (lines.length%po_ile_firm)); j+=po_ile_firm){
+            firmy = []; // zerujemy listę poprzednich firm
+            company_start = j+1; // zmieniamy numerację startową na obecną
+            for(let i=0; i<po_ile_firm; i++){ 
+
+                // Tablica firm przesyłana do funkcji
+                firmy.push(lines[i].split('&').join('%26')); // trzeba zastąpić wszystkie '&' kodem URL
+            }
+
+            GenerujDane(firmy); // Generujemy dane i wyświetlamy na stronie
         }
 
-        GenerujDane(firmy);
+        // Generujemy pozostałe firmy (o ile istnieją)
+        if(lines.length%po_ile_firm != 0){
+            firmy = [];
+            company_start += po_ile_firm;
+            
+            for(let i=company_start-1; i<lines.length; i++){
+                firmy.push(lines[i].split('&').join('%26'));
+            }
+            GenerujDane(firmy);
+        }
     };
 
     reader.onerror = (e) => alert(e.target.error.name);
 
     reader.readAsText(file);
 });
+
+const Generator = () => {
+    counter = 0;
+    company_counter = 0;
+    company_start = 1;
+    files = input[0].files;
+    $('#data-body').html("");
+
+    if(files.length == 0) return;
+
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/); // Podziel wszystkie linijki na osobne wartości array'a
+
+        let firmy = [];
+        
+        // UWAGA: ilość znaków w zapytaniu nie może przekroczyć 1000
+        let po_ile_firm = parseInt($('#po_ile_firm').val()); // pobierz ile firm brać pod uwagę w jednym zapytaniu ze strony (domyślnie 45)
+
+        for(let j=0; j<(lines.length - (lines.length%po_ile_firm)); j+=po_ile_firm){
+            firmy = []; // zerujemy listę poprzednich firm
+            company_start = j+1; // zmieniamy numerację startową na obecną
+            for(let i=0; i<po_ile_firm; i++){ 
+
+                // Tablica firm przesyłana do funkcji
+                firmy.push(lines[i].split('&').join('%26')); // trzeba zastąpić wszystkie '&' kodem URL
+            }
+
+            GenerujDane(firmy); // Generujemy dane i wyświetlamy na stronie
+        }
+
+        // Generujemy pozostałe firmy (o ile istnieją)
+        if(lines.length%po_ile_firm != 0){
+            firmy = [];
+            company_start += po_ile_firm;
+            
+            for(let i=company_start-1; i<lines.length; i++){
+                firmy.push(lines[i].split('&').join('%26'));
+            }
+            GenerujDane(firmy);
+        }
+    };
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+}
+
+$('#licz').on('click', Generator);
